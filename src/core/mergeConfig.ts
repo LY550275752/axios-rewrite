f92b0120a962af84f96db63e78828e1fd452ed4e
Sauce <550275752@qq.com>
2020-04-26 08:55:35 +0800

    init formValStrat2

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -1,3 +1,10 @@
 function defaultStrat(val1: any, val2: any): any {
     return typeof val2 !== undefined ? val2 : val1;
 }
+
+// 与接口业务强相关字段，只能采用自定义配置
+function formValStrat(val1: any, val2: any): any {
+    if (typeof val2 !== undefined) {
+        return val2;
+    }
+}
